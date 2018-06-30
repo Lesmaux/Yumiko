3262732bc420c6841bf69d02e156a929c0f42253
lesmaux <lesmauxnz@gmail.com>
2018-07-01 11:34:05 +1200

    - Added support for multiple messages Now can edit an array of messages across servers. also made some minor visual changes and removed unused code. Added some gcloud files.

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const request = require("request");
 const second = 1000;
+const pages = 10;
 let discordMessages = [];
 let lastMsg;
 let data = {};
 let recent = {};
 let clock = 0;
+
 setInterval(function() {
+    //runs get data every 60 seconds
     if(clock % 60 === 0) {
-        console.log("I am doing my 5 minutes check");
-        getData(0, 6);
+        getData(0, pages-1);
     }
     clock++
 }, second);
@@ -32,7 +34,6 @@ function getData(currentPage, pages){
             } else{
                 updateRecent();
                 updateDiscord();
-                console.log(data)
             }
         }
     })
@@ -66,25 +67,27 @@ function updateDiscord(){
                 "description": "Displaying players in the top 300 who have played ranked in the last 30 minutes.\n- ",
                 // "url": "https://discordapp.com",
                 "color": 16743647,
+                "timestamp": new Date(),
                 "footer": {
 
-                    "text": "updated"
+                    // "text": "updated"
                 },
                 "thumbnail": {
                     "url": "https://i.imgur.com/LmdHZUg.png"
                 },
                 "author": {
-                    "name": "lesmaux",
-                    "url": "https://discordapp.com",
-                    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+                    // "name": "lesmaux",
+                    // "url": "https://discordapp.com",
+                    // "icon_url": "https://cdn.discordapp.com/avatars/101209482347937792/a_d309df2dcf75c67d9d5777fca7327bf3.png?size=128"
                 },
                 "fields": fields
             }
         }
     ;
-
-    if (lastMsg !== undefined) {
-        lastMsg.edit(embed);
+    for(let i = 0, len = discordMessages.length; i < len ; i++){
+        if (discordMessages[i] !== undefined) {
+            discordMessages[i].edit(embed);
+        }
     }
 }
 
@@ -130,134 +133,34 @@ function updateList(obj){
     }
 }
 
-
 client.on("ready", async () => {
-    // This event will run if the bot starts, and logs in, successfully.
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-// Example of changing the bot's playing game to something useful. `client.user` is what the
-// docs refer to as the "ClientUser".
-//client.user.setActivity(`Serving ${client.guilds.size} servers`);
-
-});
-
-client.on("guildCreate", guild => {
-    // This event triggers when the bot joins a guild.
-    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
-
-client.on("guildDelete", guild => {
-    // this event triggers when the bot is removed from a guild.
-    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 
 client.on("message", async message => {
-    // This event will run on every single message received, from any channel or DM.
 
-    // It's good practice to ignore other bots. This also makes your bot ignore itself
-    // and not get into a spam loop (we call that "botception").
     if(message.author.bot) return;
 
-    // Also good practice to ignore any message that does not start with our prefix,
-    // which is set in the configuration file.
     if(message.content.indexOf(config.prefix) !== 0) return;
 
-    // Here we separate our "command" name, and our "arguments" for the command.
-    // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-    // command = say
-    // args = ["Is", "this", "the", "real", "life?"]
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    // Let's go with a few common example commands! Feel free to delete or change those.
-
-    if(command === "ping") {
-        // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-        // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-        const m = await message.channel.send("Ping?");
-        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    }
-
-    if(command === "say") {
-        // makes the bot say something and delete the message. As an example, it's open to anyone to use.
-        // To get the "message" itself we join the `args` back into a string with spaces:
-        const sayMessage = args.join(" ");
-        // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-        message.delete().catch(O_o=>{});
-        // And we get the bot to say the thing:
-        message.channel.send(sayMessage);
-    }
-
     if(command === "here!") {
-        lastMsg = await message.channel.send("OK!");
-
-    }
-
-
-    if(command === "kick") {
-        // This command must be limited to mods and admins. In this example we just hardcode the role names.
-        // Please read on Array.some() to understand this bit:
-        // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-        if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+        //Only allows command from certain roles
+        if(!message.member.roles.some(r=>["Admin", "Yumiko"].includes(r.name)) )
         return message.reply("Sorry, you don't have permissions to use this!");
 
-        // Let's first check if we have a member and if we can kick them!
-        // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-        // We can also support getting the member by ID, which would be args[0]
-        let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-        if(!member)
-            return message.reply("Please mention a valid member of this server");
-        if(!member.kickable)
-            return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-
-        // slice(1) removes the first part, which here should be the user mention or ID
-        // join(' ') takes all the various parts to make it a single string.
-        let reason = args.slice(1).join(' ');
-        if(!reason) reason = "No reason provided";
-
-        // Now, time for a swift kick in the nuts!
-        await member.kick(reason)
-            .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-        message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-    }
-
-    if(command === "ban") {
-        // Most of this command is identical to kick, except that here we'll only let admins do it.
-        // In the real world mods could ban too, but this is just an example, right? ;)
-        if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
-        return message.reply("Sorry, you don't have permissions to use this!");
-
-        let member = message.mentions.members.first();
-        if(!member)
-            return message.reply("Please mention a valid member of this server");
-        if(!member.bannable)
-            return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-
-        let reason = args.slice(1).join(' ');
-        if(!reason) reason = "No reason provided";
-
-        await member.ban(reason)
-            .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-        message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-    }
-
-    if(command === "purge") {
-        // This command removes all messages from all users in the channel, up to 100.
-
-        // get the delete count, as an actual number.
-        const deleteCount = parseInt(args[0], 10);
-
-        // Ooooh nice, combined conditions. <3
-        if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-            return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-
-        // So we get our messages, and delete them. Simple enough, right?
-        const fetched = await message.channel.fetchMessages({limit: deleteCount});
+        //Deletes last 100 messages in channel
+        const fetched = await message.channel.fetchMessages({limit: 100});
         message.channel.bulkDelete(fetched)
             .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+
+        //sends a message and adds it to the edit list.
+        lastMsg = await message.channel.send("OK!");
+        discordMessages.push(lastMsg);
+        updateDiscord()
     }
 });
 
